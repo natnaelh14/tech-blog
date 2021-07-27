e91200ec61e93710bcbe0d95b7ca90176995130c
Natnael Haile <natnaelh14@github.com>
2021-07-27 16:15:49 -0400

    created hooks for model

--- models/User.js
+++ models/User.js
@@ -24,6 +24,21 @@ User.init(
     },
   },
   {
+    //Hooks are methods that hook to a model.
+    // When adding hooks via the init() method, they go below
+    hooks: {
+      // Use the beforeCreate hook to work with data before a new instance is created
+      beforeCreate: async (newUserData) => {
+        // In this case, we are taking the user's email address, and making all letters lower case before adding it to the database.
+        newUserData.email = await newUserData.email.toLowerCase();
+        return newUserData;
+      },
+      // Here, we use the beforeUpdate hook to make all of the characters lower case in an updated email address, before updating the database.
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.email = await updatedUserData.email.toLowerCase();
+        return updatedUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
