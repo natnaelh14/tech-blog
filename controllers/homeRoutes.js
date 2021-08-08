cf66eacc2d1050bd629083a0f7815f150e8b0630
natnaelh14 <natnaelh14@github.com>
2021-08-08 17:35:50 -0400

    created update and delete button

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,25 +1,15 @@
 const router = require("express").Router();
-const { Blog, User } = require("../models/");
+const { Blog } = require("../models/");
 const withAuth = require("../utils/auth");
 
 router.get('/', withAuth, async (req, res) => {
   try {
-
     const postData = await Blog.findAll({
       where: {
         user_id: req.session.user_id,
       },
     });
-    console.log('hello')
-    const userData = await User.findAll({
-      where: {
-        id: req.session.user_id,
-      },
-    });
     const posts = await postData.map((post) => post.get({ plain: true }));
-    console.log(posts)
-    // const user = await userData.get({ plain: true });
-    // console.log(user)
     res.render("homepage", {
       posts,
       logged_in: true,
